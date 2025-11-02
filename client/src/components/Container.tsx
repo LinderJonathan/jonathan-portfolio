b4616d313ece675f3258987217db3f27bc138fda
Jonathan Linder <linderjonathan3@gmail.com>
2025-11-02 22:56:17 +0100

    containerblock done

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -2,16 +2,38 @@ import '../App.css'
 import '../components/ContainerBlock'
 
 import type {containerBlockProps} from "../components/ContainerBlock"
+import ContainerBlock from '../components/ContainerBlock'
 
 type containerProps = {blocks: ({ id: string } & containerBlockProps)[]}
 
 function Container({blocks}: containerProps) {
+
+  const renderedBlocks = []
+  var k = 0
+
+  // TODO: see if this works, give styling
+  for (const block of blocks) {
+
+    renderedBlocks.push(
+    <div className={k%2 === 0 ? "ContainerBlockLeft" : "ContainerBlockRight"}>
+      <ContainerBlock 
+        key={block.id} 
+        description={block.description}
+        subtitle={block.subtitle}
+        imageSource={block.imageSource}
+      />
+    </div>
+    )
+
+    k++
+  }
+  
   return (
     <>
       <div className="Container">
         <h1 id="message" className="home-message">Hi, I'm Jonathan</h1>
       </div>
-      // TODO: loop over blocks
+      {renderedBlocks}
     </>
   )
 }
--- client/src/components/ContainerBlock.tsx
+++ client/src/components/ContainerBlock.tsx
@@ -9,19 +9,16 @@ export type containerBlockProps = {
 function ContainerBlock({description, subtitle, imageSource}:containerBlockProps) {
     return (
         <>
-            <div className="containerBlock">
-                {imageSource && (
-                    <img
-                        src={imageSource}
-                        alt="../assets/profileCropped.jpg"
-                    />
-                )}
-                {subtitle && (<div>{subtitle}</div>)}
+            {imageSource && (
+                <img
+                    src={imageSource}
+                    alt="../assets/profileCropped.jpg"
+                />
+            )}
+            {subtitle && (<div>{subtitle}</div>)}
 
-                <div className="containerBlockDescription">
-                    { description }
-                </div>
-
+            <div className="containerBlockDescription">
+                { description }
             </div>
         </>
     )
