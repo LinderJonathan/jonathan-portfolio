abff870f97368a4c92cba6b26629d782f5caf6ac
Jonathan Linder <linderjonathan3@gmail.com>
2025-12-22 22:31:29 +0100

    started on home page descriptions

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,17 +4,22 @@ function Home() {
     return (
         <>
             <Container
-                pageMessage='Hi, Im Jonathan'
                 blocks={[
                     {
                         id: '1',
-                        header: 'Software Engineering',
+                        header: 'Hi, Im Jonathan',
+                        isCentered: true,
+                        description:'... And I am 25 years old from Huskvarna, Sweden. With a great interest in technology and sense of problem solving, I Relocated in 2020 to Gothenburg to pursue Computer Science and Engineering at Chalmers University of Technology. Having graduated with a B.Sc. in computer science and M.Sc. in algorithms and logic...'
+                    },
+                    {
+                        id: '2',
+                        header: '... Software Engineering...',
                         isCentered: true,
                         description:''
                     },
                     {
-                        id: '2',
-                        header: 'Embedded Development',
+                        id: '3',
+                        header: '... Embedded Development...',
                         isCentered: true,
                         description: ''
                     }
